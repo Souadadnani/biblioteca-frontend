821acf50ca6721461216986e60151068a26632dc
Souad <adnanisouad21@gmail.com>
2024-03-05 23:30:29 +0100

    ya

--- src/componentes/usuarios/Logout.jsx
+++ src/componentes/usuarios/Logout.jsx
@@ -1,9 +1,12 @@
+import { useNavigate } from "react-router-dom";
 
 export default function Logout({lector, setLector}){
 
+    const navigate = useNavigate();
     const cerrarSesion = () =>{
         setLector(null);
         localStorage.removeItem('token');
+        navigate("/");
     }
     return(
         <>
